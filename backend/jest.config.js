38c7c138d402e44d25e586bfd3e25b7a70dacf5b
edehvictor <edehvictor715@gmail.com>
2026-04-23 16:07:08 +0100

    fix: make backend tests CI-ready

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   testEnvironment: "node",
   roots: ["<rootDir>/src"],
   testMatch: ["**/__tests__/**/*.test.ts"],
+  setupFiles: ["<rootDir>/src/__tests__/setup.ts"],
   collectCoverageFrom: [
     "src/**/*.ts",
     "!src/**/*.d.ts",
